e1b21841ca62412f34d7972ca2ab1d93dba2ee08
Matheus Mota <75027532+reitheus@users.noreply.github.com>
2026-05-15 11:35:06 -0300

    Delete src directory

--- src/events/ready.js
+++
@@ -1,9 +0,0 @@
-import { refreshTaskboard } from '../utils/taskboard.js';
-
-export const name  = 'ready';
-export const once  = true;
-
-export async function execute(client) {
-  console.log(`✅ Bot online como ${client.user.tag}`);
-  await refreshTaskboard(client);
-}
